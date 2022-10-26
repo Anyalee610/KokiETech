3181d2cc6a669813ac1214c0c33a4e5ecf8fd929
Anyalee610 <anayalambright610@gmail.com>
2022-10-26 15:50:14 -0400

    added event function'

--- sign_up_page/sign_up.js
+++ sign_up_page/sign_up.js
@@ -1,9 +1,17 @@
+//dom elements 
+const submitButton = document.getElementById("submit");
+
+//event listener function 
+const clickButton = () => {
+    fetch('')
+}
+
+
 //background color
 document.body.style.backgroundColor = "#163919";
 document.body.style.textAlign = "center";
 
-//creating varibles
-const h1 = document.getElementById('h1')
 
-//varible styles
+
+
 
