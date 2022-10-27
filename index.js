7bde104b6884fe8b165b88d571f9f08785527060
Celybot <youancece@gmail.com>
2022-10-27 17:28:48 -0400

    created transtion home to sign up

---
+++ index.js
@@ -0,0 +1,7 @@
+const signUp = document.getElementById('btn')
+
+const openHome = function() {
+    document.location.href = "sign_up_page/sign_up.html";
+}
+ 
+signUp.addEventListener('click', openHome)
