ae9a13981736a0d1baeb9ab3a4295a007e7820ee
Anyalee610 <anayalambright610@gmail.com>
2023-01-10 18:35:31 -0500

    all done

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 //get the postgres database from the 
 const pool = new Pool({
-    database: 'KokiETech',
+    database: 'railway',
     user:     '',
     password: ''
 })
